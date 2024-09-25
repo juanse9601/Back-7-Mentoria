72e808dbfd0a9ad2e47c76c582f800dc81dc94ca
juanse9601 <benice9601@gmail.com>
2024-09-25 11:31:24 -0500

    Add files via upload

---
+++ src/Clases de Mentoria/demo/src/main/java/com/Excepciones/demo/Excepciones/NombreUsuarioInvalidoException.java
@@ -0,0 +1,8 @@
+package com.Excepciones.demo.Excepciones;
+
+public class NombreUsuarioInvalidoException extends UsuarioException {
+
+    public NombreUsuarioInvalidoException(String message) {
+        super(message);
+    }
+}
---
+++ src/Clases de Mentoria/demo/src/main/java/com/Excepciones/demo/Excepciones/UsuarioException.java
@@ -0,0 +1,9 @@
+package com.Excepciones.demo.Excepciones;
+
+public class UsuarioException extends RuntimeException{
+
+    public UsuarioException(String message){
+        super(message);
+    }
+
+}
